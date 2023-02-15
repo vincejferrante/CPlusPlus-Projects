4ff7a4c5e52b0ca54c9e5ceab7e42b437bf107bf
Vincent Ferrante <83679092+vincejferrante@users.noreply.github.com>
2023-02-14 20:04:20 -0800

    Create Drill.cpp

---
+++ chapter5/Drill.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    //Cout syntax error
+    
+    //cout << "success!\n" missing ;
+    //cout<< "success" << !\n"; missing a parenthesis
+    //count << success << '\n; missing parenthesis
+    int res = 7;
+    vector<int> v(10);
+    v[5] = res;
+    cout << "success!\n";
+
+}
