109bd329bd4823c76db44014c312726e9834f325
Vincent Ferrante <83679092+vincejferrante@users.noreply.github.com>
2023-02-15 21:40:01 -0800

    Update Drill.cpp

--- chapter5/Drill.cpp
+++ chapter5/Drill.cpp
@@ -4,14 +4,24 @@
 using namespace std;
 
 int main() {
+    //Exercises 1 to 5
     //Cout syntax error
-    
     //cout << "success!\n" missing ;
     //cout<< "success" << !\n"; missing a parenthesis
     //count << success << '\n; missing parenthesis
+    //Exercise six
     int res = 7;
     vector<int> v(10);
     v[5] = res;
     cout << "success!\n";
+    //Exercise seven
+    bool cond = true;
+    if(cond)
+    {
+        cout << "Success!\n";
+    }
+    else{
+        cout << "Fail!\n";
+    }
 
 }
